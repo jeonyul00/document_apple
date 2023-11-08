f8792ef6d3a74d02bffe898409c0d031a1768850
jeon yul <jeonyul.citylabs@gmail.com>
2023-11-08 18:41:34 +0900

    section 1

--- 01_GettingStart/04_GettingStarted/04_GettingStarted/ViewController.swift
+++ 01_GettingStart/04_GettingStarted/04_GettingStarted/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 아울렛
+    // @IBOutlet : 이 변수가 인터페이스빌더(스토리보드)로 부터 온 것을 의미
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myButton.setTitle("onPress", for: .normal)
     }
-
+    
+    // 액션
+    // sender : 이 함수가 반드시 1개의 버튼에만 연결되는건 아니다. 그 경우 이 함수를 실행시킨 오브젝트가 sender로 넘어옴
+    @IBAction func onPress(_ sender: Any) {
+        print("the button was press")
+    }
+    
 
 }
-
+ 
